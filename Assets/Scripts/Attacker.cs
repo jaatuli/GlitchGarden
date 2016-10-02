b5895511a9349fe91bdebea4f680324c400fb6ce
Eero Karhu <eero.s.karhu@iki.fi>
2016-10-02 11:36:38 +0300

    Collision and animation events

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+    private float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime, Space.World);
+	}
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log(name + " trigger enter");
+    }
+
+    public void setSpeed (float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget (float damage)
+    {
+        Debug.Log(name + " caused damage " + damage);
+    }
+}
