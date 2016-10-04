bd7152364735eb6f3318c8f301e6fe0fc41c08e3
Eero Karhu <eero.s.karhu@iki.fi>
2016-10-04 10:08:23 +0300

    Defender spawn buttons

---
+++ Assets/Scripts/DefenderSpawn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderSpawn : MonoBehaviour {
+
+    public GameObject defenderPrefab;
+
+    private DefenderSpawn[] buttonArray;
+    public static GameObject selectedDefender;
+
+	// Use this for initialization
+	void Start () {
+        buttonArray = GameObject.FindObjectsOfType<DefenderSpawn>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown() {
+        foreach (DefenderSpawn thisButton in buttonArray) {
+            thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        if(defenderPrefab) {
+            selectedDefender = defenderPrefab;
+        }
+
+    }
+}
