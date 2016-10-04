9e0bdd0688aa95f50d9f68825e0801da1316bc46
Eero Karhu <eero.s.karhu@iki.fi>
2016-10-04 16:08:22 +0300

    Testing

--- Assets/Scripts/VictoryMovie.cs
+++ Assets/Scripts/VictoryMovie.cs
@@ -5,8 +5,8 @@ public class VictoryMovie : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        Handheld.PlayFullScreenMovie("Congratulations.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+    }
 	
 	// Update is called once per frame
 	void Update () {
