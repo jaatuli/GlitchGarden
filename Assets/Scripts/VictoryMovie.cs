baeeea0adfc04faac40fe4f572f731e82c616f16
Eero Karhu <eero.s.karhu@iki.fi>
2016-10-05 14:58:42 +0300

    Victory movie fixed for windows

--- Assets/Scripts/VictoryMovie.cs
+++ Assets/Scripts/VictoryMovie.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class VictoryMovie : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Handheld.PlayFullScreenMovie("Congratulations.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+    public MovieTexture movie;    
+    private AudioSource audioSource;
+    private GameObject image;
+
+    // Use this for initialization
+    void Start() {
+
+        image = GameObject.Find("Movie");
+
+        if (PlayerPrefsManager.GetDifficulty() == 3) {
+
+            if (Application.platform == RuntimePlatform.Android) {
+                Handheld.PlayFullScreenMovie("Congratulations.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+            }
+            
+            if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer) { 
+            
+                //Init
+                audioSource = GetComponent<AudioSource>();
+                image.GetComponent<RawImage>().texture = movie;
+
+                audioSource.clip = movie.audioClip;
+
+                movie.Play();
+                audioSource.Play();
+
+                
+                
+            }
+        }
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void Update() {
+
+        if (!movie.isPlaying) {
+            image.SetActive(false);
+        }
+    }
 }
